66d24391ac0787bb567d0b7c08190e6803604a5b
AlexTrutenko <o.trutenko@student.maastrichtuniversity.nl>
2025-04-14 01:56:33 +0200

    Update PatientDataGenerator.java(added Javadoc documentation)

--- src/main/java/com/cardioGenerator/generators/PatientDataGenerator.java
+++ src/main/java/com/cardioGenerator/generators/PatientDataGenerator.java
@@ -1,7 +1,17 @@
 package com.cardioGenerator.generators;
 
 import com.cardioGenerator.outputs.OutputStrategy;
-
+     /** An interface for generating simulated health data for patient.
+     * The classes that are implementing this interface are responsible for producing a specific type of patient data such as ECG, blood pressure, blood saturation, or other vital signs. 
+     * This interface provides a unified contract for invoking data generation regardless of the type.
+     * 
+     * @author Oleksandr Trutenko
+     */
 public interface PatientDataGenerator {
+     /**Generates health data for a specific patient and sends it to the provided output strategy.
+     * 
+     * @param patientId - a unique identification of a patient.
+     * @param outputStrategy - The output strategy to which the data will be sent.
+     */
     void generate(int patientId, OutputStrategy outputStrategy);
 }
