60ed493461e7e8df535edf90b825919ec968e0b0
AlexTrutenko <o.trutenko@student.maastrichtuniversity.nl>
2025-04-14 02:17:18 +0200

    Update BloodSaturationDataGenerator.java(added Javadoc documentation)

--- src/main/java/com/cardioGenerator/generators/BloodSaturationDataGenerator.java
+++ src/main/java/com/cardioGenerator/generators/BloodSaturationDataGenerator.java
@@ -3,10 +3,18 @@ package com.cardioGenerator.generators;
 import java.util.Random;
 
 import com.cardioGenerator.outputs.OutputStrategy;
-
+ /**This class generates simulated blood saturation for patients in a health monitoring system where each patient is initialized with a slightly randomized healthy baseline.
+ * The generator maintains the last known saturation value for each patient and introduces small variations on each call to simulate real-time monitoring.
+ * 
+ * @author Oleksandr Trutenko
+ */
 public class BloodSaturationDataGenerator implements PatientDataGenerator {
     private static final Random random = new Random();
     private int[] lastSaturationValues;
+    /** Constructs a new blood saturation data generator.
+     *  Initialize each patient a baseline from 95 to 100 %.
+     * @param patientCount - the total amount of patients.
+    */
 
     public BloodSaturationDataGenerator(int patientCount) {
         lastSaturationValues = new int[patientCount + 1];
@@ -16,7 +24,14 @@ public class BloodSaturationDataGenerator implements PatientDataGenerator {
             lastSaturationValues[i] = 95 + random.nextInt(6); // Initializes with a value between 95 and 100
         }
     }
-
+    /**Generates a blood saturation value for specific patient and outputs it using a giving output strategy.
+     * Saturation level ranges over time and are clamped between 90% and 100%.
+     * 
+     * 
+     * @param patientID - represents unique identification of a patient.
+     * @param outputStrategy - The output strategy to which the data will be sent.
+     * @return a blood saturation value.
+     */
     @Override
     public void generate(int patientId, OutputStrategy outputStrategy) {
         try {
