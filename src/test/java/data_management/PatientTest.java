1df06a479fdbf6cce965d2ae66d7aeacd151279e
Mykyta <m.reznikov@student.maastrichtuniversity.nl>
2025-04-19 21:22:55 +0200

    pomxml

--- src/test/java/data_management/PatientTest.java
+++ src/test/java/data_management/PatientTest.java
@@ -10,7 +10,7 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
 
 /**
- * Unit tests for {@code Pation}.
+ * Unit tests for {@code Patient}.
  *
  * This code verifies:
  *
