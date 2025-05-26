e352a937d75b7d33cf93d239671f1becefb60c28
Oleksandr Trutenko <o.trutenko@student.maastrichtuniversity.nl>
2025-05-26 04:53:09 +0200

    Modified DataReader interface to handle real-time data

--- src/main/java/com/dataManagement/DataReader.java
+++ src/main/java/com/dataManagement/DataReader.java
@@ -10,4 +10,12 @@ public interface DataReader {
      * @throws IOException if there is an error reading the data
      */
     void readData(DataStorage dataStorage) throws IOException;
+    /**
+     * Stops the data reading process gracefully.
+     * This method should close any open connections or resources used during data reading.
+     *
+     * @throws IOException if an error occurs while stopping the reading process
+     */
+    void stopReading() throws IOException;
+    
 }
